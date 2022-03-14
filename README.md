d0763ceddee1c29483524e981a893928d787f637
Barham Farraj <barhamfarraj@icloud.com>
2022-03-14 19:27:41 +0300

    Readme Updated & Documantation added

--- README.md
+++ README.md
@@ -1,17 +1,27 @@
 #  Lab 06 : Game of Greed 
 
-The game of greed pits players against one another to see who can attain 10,000 points earliest.<br>
+The **Game Of Greed** pits players against one another to see who can attain 10,000 points earliest.<br>
 As a result, we created our own version of the (dice game Game of Greed) by Python .
 
 # PR
 The Pull Request Link : [PR](https://github.com/Game-of-Greed-group1/game-of-greed-I/tree/main)
 
 
- # Contributors: (Group 1)
+# Contributors: (Group 1)
+
 1. Joud Alnsour 
+
 2. Hind Hbashneh
+
 3. Nour Eddein AlAbbasi
+
 4. Barham Farraj
+
+# Change Log
+ * Rolling Dice , Banking , Scoring ***(14/03/2022)***
+The game is played with six dice. A player starts their turn by throwing all six dice. Once the dice have been rolled the player may put aside any dice that form scoring melds (see melds below). A player must put aside at least one scoring die each throw, but beyond that is free in what dice they put aside or throw again.
+
+Players may now roll any remaining dice not already put aside in hopes of being able to make more melds, risking the chance of farkling (see below) and scoring nothing. Players who decide not to take that risk can instead bank. When a player banks any points earned during the round are added to the grand total and are safe for the rest of the game. The player's turn ends and they pass all dice to their opponent who takes their turn.
 
 **Estimate of time needed to complete: 5**
 
--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -3,11 +3,36 @@ from collections import Counter
 
 
 class GameLogic:
+    """
+    Creating a GameLogic Class Containning The logic of Our game.
+    """
     def roll_dice(n=6):
+        """
+        Creating Method to create a virtual dices and return a random number from 1 to 6 
+        this method will create ad many dices as wanted by changeing the n value.
+        For our game we will stick with 6 dices .
+        
+        This method will return a tuple of random numbers with length of 6 "number of dices"
+
+        """
         return tuple([randint(1, 6) for _ in range(n)])
 
     @staticmethod
     def calculate_score(dices):
+        """
+        Creating a method  for making the rules of our game .
+        This method will take the tuple of numbers of our roll_dice method as we roll the dices while playing the game.
+
+        We made a dictionary of the rules so we can access them.
+        Each value of the dice and how many repetition of the value have their own case.
+
+        In the following code will checkk for specific rule followed from a link will attach below this doc string.
+
+        At the end the method will return the score which is calcuated as the rules saved in the dictionary. 
+        
+        //search for the counte method and most_common if never heared of them
+        """
+        # (https://en.wikipedia.org/wiki/Dice_10000)
         rules = {
             1: {1: 100, 2: 200, 3: 1000, 4: 2000, 5: 3000, 6: 4000},
             2: {1: 0, 2: 0, 3: 200, 4: 400, 5: 600, 6: 800},
@@ -34,6 +59,33 @@ class GameLogic:
             score += rules[i[0]][i[1]]
         return score
 
+class Banker :
+    """
+    Define a Banker class to handle the scoring algorithm during the game.
+    have two proprties balance, shelved and assign them to 0 for now.
+    """
+
+    def __init__(self):
+        self.balance=0
+        self.shelved=0
+    """
+    Defining a method called shelf to store the score while playing the game and before saving the round and assign it to the shelved property 
+    """
+    def shelf(self,point):                  
+        self.shelved+=point
+    """
+    Defining a method called Bank to score the latest round score "from the shelved property " and add it the previous rounds score and assign it to the balance proprty. 
+    
+    """
+    def bank(self): 
+        self.balance+=self.shelved 
+        self.clear_shelf() 
+
+    """
+    Defining a clear_shelf method to re assign the value in the shlved property to 0 . this way will let the next round shelved score equal to zero.
+    """
+    def clear_shelf(self):                      
+        self.shelved=0
 
 if __name__ == "__main__":
     game_logic = GameLogic()
