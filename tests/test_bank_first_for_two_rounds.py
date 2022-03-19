a7953e1f395ed9b589dc4e824a1d340f567d070d
noureddein <noureddein@gmail.com>
2022-03-19 20:56:54 +0300

    last edit

--- tests/test_bank_first_for_two_rounds.py
+++ tests/test_bank_first_for_two_rounds.py
@@ -5,4 +5,3 @@ from game_of_greed.game_2 import Game
 def test_one_and_done():
     Game.rolled_dices = [[1, 4, 4, 6, 1, 6], [3, 4, 6, 1, 3, 6], [3, 4, 1, 4, 3, 5]]
     Flo.test("../tests/flow/bank_first_for_two_rounds.sim.txt")
-    pass
